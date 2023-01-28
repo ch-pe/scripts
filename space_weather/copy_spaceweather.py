d793b6273f26838b696f30aaf528a9e923b4a61d
Christian P <56875641+ch-pe@users.noreply.github.com>
2023-01-29 00:58:12 +0100

    Update copy_spaceweather.py
    added algorithm to loop over the days and read the Kp indices

--- space_weather/copy_spaceweather.py
+++ space_weather/copy_spaceweather.py
@@ -16,15 +16,20 @@ tableBlock = answer.split("\\n\\n")[3]
 #print (tableBlock)
 table = [i.split() for i in tableBlock.split('\\n')]
 
-
-#Day 1
+#todo: will need to change this at some point *shrug*
 year = 2023
-month = months.index(table[0][0]) + 1
-day = int(table[0][1])
-time_start = 0 #UTC
-time_end = 3 #UTC
-Kp_index = table [1][1]
 
-print ("On " + str(year) + "-" + str(month) + "-" + str(day) + " from "
-    + str(time_start) + ":00:00 (UTC) to " + str(time_end)
-    + ":00:00 (UTC) the expected Kp index is " + str(Kp_index))
+# loop over days
+days = [int(table[0][1]),int(table[0][3]),int(table[0][5])]
+
+for day_index, day in enumerate(days):
+    month = months.index(table[0][day_index*2]) + 1
+    for time_index in range(8):
+        Kp_index = table[time_index + 1][day_index + 1]
+        start_time = time_index * 3
+        end_time = (time_index + 1) * 3
+        print("On "+str(year)+"-"+str(month)+"-"+str(day)+", between " +
+              str(start_time) + "UTC and " + str(end_time) +
+              "UTC, the expected Kp_index is " + Kp_index)
+
+
