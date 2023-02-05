3eadc37b3b535d1a01dbb1926c9f9a79f07bf548
Christian P <56875641+ch-pe@users.noreply.github.com>
2023-02-05 17:14:27 +0100

    Update copy_spaceweather.py
    change logging

--- space_weather/copy_spaceweather.py
+++ space_weather/copy_spaceweather.py
@@ -1,16 +1,29 @@
 import urllib.request
+import caldav
 
-print("spaceweather copy script, v1.0")
+# preparation - print to cmd or log to file
+logfile = ''#"spaceweatherToCalendar.log" # empty string for cmd printing
+def log(text):
+    if logfile == '':
+        print(text)
+    else:
+        with open(logfile, 'a') as f:
+            f.write(str(text)+'\n')
+        #end open file
+    #end if/else
+#end definition
 
-# preparation
+log("\n\nspaceweather copy script, v1.0")
+
+
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
           'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 space_weather_url = "https://services.swpc.noaa.gov/text/3-day-forecast.txt"
-print("opening " + space_weather_url)
+log("opening " + space_weather_url)
 page = urllib.request.urlopen(space_weather_url)
 answer = str(page.read())
-print(answer)
+log(answer)
 
 tableBlock = answer.split("\\n\\n")[3]
 #print (tableBlock)
@@ -20,16 +33,20 @@ table = [i.split() for i in tableBlock.split('\\n')]
 year = 2023
 
 # loop over days
+entries = []
 days = [int(table[0][1]),int(table[0][3]),int(table[0][5])]
 
 for day_index, day in enumerate(days):
     month = months.index(table[0][day_index*2]) + 1
     for time_index in range(8):
-        Kp_index = table[time_index + 1][day_index + 1]
+        Kp_index = float(table[time_index + 1][day_index + 1])
         start_time = time_index * 3
-        end_time = (time_index + 1) * 3
-        print("On "+str(year)+"-"+str(month)+"-"+str(day)+", between " +
-              str(start_time) + "UTC and " + str(end_time) +
-              "UTC, the expected Kp_index is " + Kp_index)
-
+        end_time = (time_index + 1) * 3 - 1 # idea is to go up to 2:59
+        log("On "+str(year)+"-"+str(month)+"-"+str(day)+", between " +
+            str(start_time) + ":00 UTC and " + str(end_time) +
+            ":59 UTC, the expected Kp_index is " + str(Kp_index))
+        entries.append([year,month,day,start_time,Kp_index])
+        # create a calendar entry, append to list
+    #end loop times
+#end loop days
 
