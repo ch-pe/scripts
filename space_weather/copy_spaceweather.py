f750791724cf20a12538b0ebc2b8676537ef38ff
Christian P <56875641+ch-pe@users.noreply.github.com>
2023-03-20 23:24:37 +0100

    first working version
    creates the events in the online calendar.
    now I just need to filter for interesting values

--- space_weather/copy_spaceweather.py
+++ space_weather/copy_spaceweather.py
@@ -1,9 +1,9 @@
 import urllib.request
 import caldav
-import passwordsAndStuff.py
+import passwordsAndStuff
 
 # preparation - print to cmd or log to file
-logfile = ''#"spaceweatherToCalendar.log" # empty string for cmd printing
+logfile = "spaceweatherToCalendar.log" # empty string for cmd printing
 def log(text):
     if logfile == '':
         print(text)
@@ -31,22 +31,22 @@ tableBlock = answer.split("\\n\\n")[3]
 table = [i.split() for i in tableBlock.split('\\n')]
 
 #todo: will need to change this at some point *shrug*
-year = 2023
+year = "2023"
 
 # loop over days
 entries = []
-days = [int(table[0][1]),int(table[0][3]),int(table[0][5])]
+days = [table[0][1],table[0][3],table[0][5]]
 
 for day_index, day in enumerate(days):
-    month = months.index(table[0][day_index*2]) + 1
+    month = str(months.index(table[0][day_index*2]) + 1).rjust(2,"0")
     for time_index in range(8):
-        Kp_index = float(table[time_index + 1][day_index + 1])
-        start_time = time_index * 3
-        end_time = (time_index + 1) * 3 - 1 # idea is to go up to 2:59
-        log("On "+str(year)+"-"+str(month)+"-"+str(day)+", between " +
-            str(start_time) + ":00 UTC and " + str(end_time) +
-            ":59 UTC, the expected Kp_index is " + str(Kp_index))
-        entries.append([year,month,day,start_time,Kp_index])
+        Kp_index = table[time_index + 1][day_index + 1]
+        start_time = str(time_index * 3).rjust(2,"0")
+        end_time = str((time_index + 1) * 3 - 1).rjust(2,"0") # idea is to go up to 2:59
+        log("On "+year+"-"+month+"-"+day+", between " +
+            start_time + ":00 UTC and " + end_time +
+            ":59 UTC, the expected Kp_index is " + Kp_index)
+        entries.append([year,month,day,start_time, end_time,Kp_index])
         # create a calendar entry, append to list
     #end loop times
 #end loop days
@@ -57,8 +57,27 @@ url=passwordsAndStuff.calendarUrl()
 username=passwordsAndStuff.calendarUsername()
 password=passwordsAndStuff.calendarPassword()
 
+# function to create events
+def new_calendar_string(year, month, day, start_time, end_time, Kp_index):
+    # I'm open for suggestions for better ways of doing this
+    dateString = year + month + day
+
+    # begin event
+    vevent  = "BEGIN:VEVENT\n"
+    vevent += "CREATED:20230321T195612Z\n"
+    vevent += "DTSTAMP:20230321T195612Z\n"
+    vevent += "LAST-MODIFIED:20220301T112233Z\n"
+    vevent += "UID:" + dateString + "T" + start_time + "0000Z\n"
+    vevent += "DTSTART:"+dateString+"T" + start_time + "0000Z\n"
+    vevent += "DTEND:"  + dateString+"T" + end_time + "5900Z\n"
+    vevent += "STATUS:CONFIRMED\n"
+    vevent += "SUMMARY:" + Kp_index + "\n"
+    vevent += "END:VEVENT\n"
+
+    return(vevent)
+
 #get the calendar
 with caldav.DAVClient(url=url, username=username, password=password) as client:
-   cal = client.calendar(url=url)
-   the_same_calendar.events()
-# create events
+    cal = client.calendar(url=url)
+    for row in entries:
+        cal.save_event(new_calendar_string(row[0], row[1], row[2], row[3], row[4], row[5]))
